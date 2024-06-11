d5cb837cc263028dd054152ba2e2b76f0a24613b
John <89491969+dededsg@users.noreply.github.com>
2024-06-11 16:49:23 -0300

    teste java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        // Escreva o código aqui
+
+        String nome = "DEDE";
+        System.out.println("Olá, " + nome);
+    }
+}
