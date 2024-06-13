dd690feded909b21fc711f6506ce1f5fec2ff4b1
John <89491969+dededsg@users.noreply.github.com>
2024-06-13 15:18:41 -0300

    PDO
    PDO

--- PDO/src/Animais/Cachorro.java
+++ PDO/src/Animais/Cachorro.java
@@ -1,6 +1,9 @@
 package Animais;
 
 public class Cachorro {
+
+    static int numeroDeCachorro;
+
     private String nome;
 
     private String cor;
@@ -13,16 +16,74 @@ public class Cachorro {
 
     private String estadoDeEspirito;
 
-    public String getNome() {
+    //Contrutores
+    //contrutor padrão
+    public Cachorro(){}
+    //contrutores adicionados
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
 
+        numeroDeCachorro ++;
+    }
+    //métodos
+
+
+    public static int getNumeroDeCachorro() {
+        return numeroDeCachorro;
+    }
+
+    public static void setNumeroDeCachorro(int numeroDeCachorro) {
+        Cachorro.numeroDeCachorro = numeroDeCachorro;
+    }
+
+    public String getNome() {
         return this.nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getCor() {
+        return cor;
+    }
 
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 
+    public int getAltura() {
+        return altura;
+    }
 
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
 
     public void comer(){}
 
@@ -46,6 +107,13 @@ public class Cachorro {
             default: this.estadoDeEspirito = "neutro";
         }
         return this.estadoDeEspirito;
+    }
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 }
 
--- PDO/src/classesNExecutaveis.java
+++ PDO/src/classesNExecutaveis.java
@@ -3,21 +3,39 @@ import Animais.Cachorro;
 public class classesNExecutaveis {
     public static void main(String[] args) {
 
-        Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro1 = new Cachorro("Rex","Branco",25,10.5,5,"nada");
 
-        cachorro1.nome = "Puppy";
-        cachorro1.cor = "Marrom";
-        cachorro1.altura = 25;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoDoRabo = 5;
+        Cachorro cachorro2 = new Cachorro("Puppy","Branco",25,10.5,5,"nada");
 
-        //cachorro1.latir();
-        //System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
 
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
-        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
-        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
+        System.out.println("--------------------------");
+
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
+
+        // 14:29 modelagem orientada a objetos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //System.out.println(cachorro2.getNome());
+        //System.out.println(cachorro2.getPeso());
+        //System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+        //System.out.println("O cachorro está " + cachorro1.interagir("vai dormir!"));
+        //System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha"));
+        //System.out.println("O cachorro está " + cachorro1.interagir("nada"));
 
     }
 }
